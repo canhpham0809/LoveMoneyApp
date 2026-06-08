60c4025e9e134a6cd142348534ced0285b37e3ff
canhpham0809 <46082568+canhpham0809@users.noreply.github.com>
2026-06-08 15:54:00 +0000

    Deploying to gh-pages from @ canhpham0809/LoveMoneyApp@31ba64e1dd6694d27d6c4d5e6d8e12cc618fade5 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1471240920" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3197435659" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
